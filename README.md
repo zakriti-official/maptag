a99adc7258852a5d980d8f345bfeb33f7408a1bc
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2024-03-05 00:11:18 +0530

    Added all map tag files

---
+++ README.md
@@ -0,0 +1,2 @@
+### Todos
+- [ ] RLS on supabase
---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+useHead({
+  script: [
+			{ src: "https://kit.fontawesome.com/6383085084.js" }
+		]
+});
+</script>
+
